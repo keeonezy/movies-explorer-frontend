47732584f3ed0667f8c84c1b972e9042c3ab18c2
Alexander Malukhin <keeonenew@yandex.ru>
2023-08-28 11:41:08 +0300

    feat: add movies card

---
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -0,0 +1,55 @@
+import "./MoviesCard.css";
+import moviesCard1 from "../../images/movies-card-1.jpg";
+import moviesCard2 from "../../images/movies-card-2.jpg";
+
+function MoviesCard() {
+    return (
+        <>
+            <li className="movies-card">
+                <div className="movies-card__container">
+                    <div className="movies-card__container-left">
+                        <h2 className="movies-card__title">33 слова о дизайне</h2>
+                        <p className="movies-card__description">1ч 47м</p>
+                    </div>
+                    <div className="movies-card__save"></div>
+                </div>
+                <img src={moviesCard1} alt="" className="movies-card__image" />
+            </li>
+
+            <li className="movies-card">
+                <div className="movies-card__container">
+                    <div className="movies-card__container-left">
+                        <h2 className="movies-card__title">33 слова о дизайне</h2>
+                        <p className="movies-card__description">1ч 47м</p>
+                    </div>
+                    <div className="movies-card__save"></div>
+                </div>
+                <img src={moviesCard2} alt="" className="movies-card__image" />
+            </li>
+
+            <li className="movies-card">
+                <div className="movies-card__container">
+                    <div className="movies-card__container-left">
+                        <h2 className="movies-card__title">33 слова о дизайне</h2>
+                        <p className="movies-card__description">1ч 47м</p>
+                    </div>
+                    <div className="movies-card__save movies-card__save-active"></div>
+                </div>
+                <img src={moviesCard1} alt="" className="movies-card__image" />
+            </li>
+
+            <li className="movies-card">
+                <div className="movies-card__container">
+                    <div className="movies-card__container-left">
+                        <h2 className="movies-card__title">33 слова о дизайне</h2>
+                        <p className="movies-card__description">1ч 47м</p>
+                    </div>
+                    <div className="movies-card__save"></div>
+                </div>
+                <img src={moviesCard1} alt="" className="movies-card__image" />
+            </li>
+        </>
+    )
+}
+
+export { MoviesCard };
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,10 +1,13 @@
 import "./MoviesCardList.css";
+import { MoviesCard } from "../MoviesCard/MoviesCard";
 
 function MoviesCardList() {
     return (
         <>
             <div className="movies-card-list">
-                <ul className="movies-card-list__cards"></ul>
+                <ul className="movies-card-list__cards">
+                    <MoviesCard />
+                </ul>
             </div>
 
             <button className="movies-card-list__button">Ещё</button>
