ac0da6916555093fc4b9e0db12c5ed282eb72b5f
Alexander Malukhin <keeonenew@yandex.ru>
2023-08-28 12:13:54 +0300

    feat: add routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import "./App.css"
-import { Header } from "../Header/Header";
-import { Footer } from "../Footer/Footer";
 import { Main } from "../Main/Main";
 import { NotFound } from "../NotFound/NotFound";
 import { Register } from "../Register/Register";
@@ -10,17 +10,20 @@ import { Movies } from "../Movies/Movies";
 import { SavedMovies } from "../SavedMovies/SavedMovies";
 
 function App() {
+  const [loggedIn, setLoggedIn] = React.useState(false);
+
   return (
     <div className="body">
       <div className="page">
-        <Header />
-        {/* <Main /> */}
-        {/* <Footer /> */}
-        {/* <NotFound /> */}
-        {/* <Register /> */}
-        {/* <Login /> */}
-        {/* <Profile /> */}
-        <SavedMovies />
+        <Routes>
+          <Route path="/" element={< Main />}></Route>
+          <Route path="/signin" element={< Login />}></Route>
+          <Route path="/signup" element={< Register />}></Route>
+          <Route path="/profile" element={< Profile />}></Route>
+          <Route path="/movies" element={< Movies />}></Route>
+          <Route path="/saved-movies" element={< SavedMovies />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
       </div>
     </div>
   );
