355c78d6e43db7b59f984ca2a6bd844bf6761cb7
Alexander Malukhin <keeonenew@yandex.ru>
2023-08-30 13:18:01 +0300

    refactor movies with pixel perfect

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -48,6 +48,94 @@ function MoviesCard() {
                 </div>
                 <img src={moviesCard1} alt="Постер фильма" className="movies-card__image" />
             </li>
+
+            <li className="movies-card">
+                <div className="movies-card__container">
+                    <div className="movies-card__container-left">
+                        <h2 className="movies-card__title">33 слова о дизайне</h2>
+                        <p className="movies-card__description">1ч 47м</p>
+                    </div>
+                    <div className="movies-card__save movies-card__save-remove"></div>
+                </div>
+                <img src={moviesCard1} alt="Постер фильма" className="movies-card__image" />
+            </li>
+
+            <li className="movies-card">
+                <div className="movies-card__container">
+                    <div className="movies-card__container-left">
+                        <h2 className="movies-card__title">33 слова о дизайне</h2>
+                        <p className="movies-card__description">1ч 47м</p>
+                    </div>
+                    <div className="movies-card__save movies-card__save-remove"></div>
+                </div>
+                <img src={moviesCard1} alt="Постер фильма" className="movies-card__image" />
+            </li>
+
+            <li className="movies-card">
+                <div className="movies-card__container">
+                    <div className="movies-card__container-left">
+                        <h2 className="movies-card__title">33 слова о дизайне</h2>
+                        <p className="movies-card__description">1ч 47м</p>
+                    </div>
+                    <div className="movies-card__save movies-card__save-remove"></div>
+                </div>
+                <img src={moviesCard1} alt="Постер фильма" className="movies-card__image" />
+            </li>
+
+            <li className="movies-card">
+                <div className="movies-card__container">
+                    <div className="movies-card__container-left">
+                        <h2 className="movies-card__title">33 слова о дизайне</h2>
+                        <p className="movies-card__description">1ч 47м</p>
+                    </div>
+                    <div className="movies-card__save movies-card__save-remove"></div>
+                </div>
+                <img src={moviesCard1} alt="Постер фильма" className="movies-card__image" />
+            </li>
+
+            <li className="movies-card">
+                <div className="movies-card__container">
+                    <div className="movies-card__container-left">
+                        <h2 className="movies-card__title">33 слова о дизайне</h2>
+                        <p className="movies-card__description">1ч 47м</p>
+                    </div>
+                    <div className="movies-card__save movies-card__save-remove"></div>
+                </div>
+                <img src={moviesCard1} alt="Постер фильма" className="movies-card__image" />
+            </li>
+
+            <li className="movies-card">
+                <div className="movies-card__container">
+                    <div className="movies-card__container-left">
+                        <h2 className="movies-card__title">33 слова о дизайне</h2>
+                        <p className="movies-card__description">1ч 47м</p>
+                    </div>
+                    <div className="movies-card__save movies-card__save-remove"></div>
+                </div>
+                <img src={moviesCard1} alt="Постер фильма" className="movies-card__image" />
+            </li>
+
+            <li className="movies-card">
+                <div className="movies-card__container">
+                    <div className="movies-card__container-left">
+                        <h2 className="movies-card__title">33 слова о дизайне</h2>
+                        <p className="movies-card__description">1ч 47м</p>
+                    </div>
+                    <div className="movies-card__save movies-card__save-remove"></div>
+                </div>
+                <img src={moviesCard1} alt="Постер фильма" className="movies-card__image" />
+            </li>
+
+            <li className="movies-card">
+                <div className="movies-card__container">
+                    <div className="movies-card__container-left">
+                        <h2 className="movies-card__title">33 слова о дизайне</h2>
+                        <p className="movies-card__description">1ч 47м</p>
+                    </div>
+                    <div className="movies-card__save movies-card__save-remove"></div>
+                </div>
+                <img src={moviesCard1} alt="Постер фильма" className="movies-card__image" />
+            </li>
         </>
     )
 }
