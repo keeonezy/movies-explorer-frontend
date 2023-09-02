53901070beb17878c1eb43e630a37e86de95760f
Alexander Malukhin <keeonenew@yandex.ru>
2023-09-03 00:32:48 +0500

    feat: add contexts and contants

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import "./App.css"
 import { Main } from "../Main/Main";
 import { NotFound } from "../NotFound/NotFound";
@@ -8,8 +9,10 @@ import { Login } from "../Login/Login";
 import { Profile } from "../Profile/Profile";
 import { Movies } from "../Movies/Movies";
 import { SavedMovies } from "../SavedMovies/SavedMovies";
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({ name: "", email: "" });
   const [loggedIn, setLoggedIn] = React.useState(true);
 
   const navigate = useNavigate();
@@ -19,19 +22,21 @@ function App() {
   };
 
   return (
-    <div className="body">
-      <div className="page">
-        <Routes>
-          <Route path="/" element={< Main loggedIn={loggedIn} />}></Route>
-          <Route path="/signin" element={< Login />}></Route>
-          <Route path="/signup" element={< Register />}></Route>
-          <Route path="/profile" element={< Profile loggedIn={loggedIn} />}></Route>
-          <Route path="/movies" element={< Movies loggedIn={loggedIn} />}></Route>
-          <Route path="/saved-movies" element={< SavedMovies loggedIn={loggedIn} />}></Route>
-          <Route path="*" element={<NotFound onBack={backPage} />}></Route>
-        </Routes>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="body">
+        <div className="page">
+          <Routes>
+            <Route path="/" element={< Main loggedIn={loggedIn} />}></Route>
+            <Route path="/signin" element={< Login />}></Route>
+            <Route path="/signup" element={< Register />}></Route>
+            <Route path="/profile" element={< Profile loggedIn={loggedIn} />}></Route>
+            <Route path="/movies" element={< Movies loggedIn={loggedIn} />}></Route>
+            <Route path="/saved-movies" element={< SavedMovies loggedIn={loggedIn} />}></Route>
+            <Route path="*" element={<NotFound onBack={backPage} />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </CurrentUserContext.Provider>
   );
 }
 
---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const API_URL = "https://api.film.nomoreparties.co";
+// export const API_URL = "http://localhost:3000";
+
+export const MOVIES_URL = "https://api.nomoreparties.co";
+
+export const movieMessages = {
+    notFound: "Ничего не найдено.",
+    enterKeyword: "Нужно ввести ключевое слово."
+}
